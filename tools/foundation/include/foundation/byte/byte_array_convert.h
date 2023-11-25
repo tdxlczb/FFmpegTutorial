5c6ae8f615c4ef5d67e5b6211fa05bc512c06489
陈志斌 <chenzhibin@cvte.com>
2023-11-25 15:05:56 +0800

    [add]add foundation tools

---
+++ tools/foundation/include/foundation/byte/byte_array_convert.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+#include "int24_t.h"
+
+void src_int24_to_float_array(const int24_t* in, float* out, int len);
+void src_float_to_int24_array(const float* in, int24_t* out, int len);
+void src_int24_to_int32_array(const int24_t* in, int32_t* out, int len);
+void src_int32_to_int24_array(const int32_t* in, int24_t* out, int len);
---
+++ tools/foundation/include/foundation/byte/int24_t.h
@@ -0,0 +1,73 @@
+﻿#ifndef INT24_T_H
+#define INT24_T_H
+
+#include <stdint.h>
+#include <ostream>
+
+#define INT24_MIN -8388608
+#define INT24_MAX 8388607
+
+class int24_t
+{
+public:
+    int24_t();
+    int24_t(int value);
+    int24_t(const int24_t& orig);
+
+    operator bool() const;
+    operator int() const;
+    operator double() const;
+
+    int24_t& operator=(int value);
+    int24_t& operator=(const int24_t& value);
+
+    int24_t operator+() const;
+    int24_t operator+(int rvalue) const;
+    int24_t operator+(const int24_t& rvalue) const;
+    int24_t operator-() const;
+    int24_t operator-(int rvalue) const;
+    int24_t operator-(const int24_t& rvalue) const;
+    int24_t operator*(int rvalue) const;
+    int24_t operator*(const int24_t& rvalue) const;
+    int24_t operator/(int rvalue) const;
+    int24_t operator/(const int24_t& rvalue) const;
+    int24_t operator%(int rvalue) const;
+    int24_t operator%(const int24_t& rvalue) const;
+
+    int24_t operator++();
+    int24_t operator++(int not_used);
+    int24_t operator--();
+    int24_t operator--(int not_used);
+
+    void operator+=(int rvalue);
+    void operator+=(const int24_t& rvalue);
+    void operator-=(int rvalue);
+    void operator-=(const int24_t& rvalue);
+    void operator*=(int value);
+    void operator*=(const int24_t& rvalue);
+    void operator/=(int rvalue);
+    void operator/=(const int24_t& rvalue);
+    void operator%=(int rvalue);
+    void operator%=(const int24_t& rvalue);
+
+    bool operator==(int rvalue) const;
+    bool operator==(const int24_t& rvalue) const;
+    bool operator!=(int rvalue) const;
+    bool operator!=(const int24_t& rvalue) const;
+    bool operator<(int rvlaue) const;
+    bool operator<(const int24_t& rvalue) const;
+    bool operator<=(int rvalue) const;
+    bool operator<=(const int24_t& rvalue) const;
+    bool operator>(int rvalue) const;
+    bool operator>(const int24_t& rvalue) const;
+    bool operator>=(int rvalue) const;
+    bool operator>=(const int24_t& rvalue) const;
+
+    friend std::ostream& operator<<(std::ostream& os, const int24_t& obj);
+
+
+private:
+    uint8_t _bytes[3];
+};
+
+#endif // INT24_T_H
